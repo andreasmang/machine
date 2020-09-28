212cc2bb00b5b23ea6b12c36f274ec64646301fb
Andreas Mang <andreas@math.uh.edu>
2020-09-27 22:38:34 -0500

    function to add noise perturbation added

---
+++ helper/addNoise.m
@@ -0,0 +1,33 @@
+function [ydelta, noise] = addNoise( y, delta )
+%ADDNOISE add noise to data
+%
+% input:
+%   y        noise free data
+%   delta    noise level
+%
+% output:
+%   ydelta   perturbed data
+%   noise    perturbation/noise added to data (delta*eta)
+
+% get size of data
+n = size( y );
+
+% compute and normalize noise
+eta = randn( n );
+eta = eta ./ norm( eta(:) );
+noise = delta.*eta;
+
+% perturb data by noise
+ydelta = y + noise;
+
+end % end of function
+
+
+
+
+%#######################################################
+% This code is part of the Matlab-based toolbox
+% MACHINE --- MAthematical and Computational metHods
+% for INverse problEms
+% For details see https://github.com/andreasmang/machine
+%#######################################################
