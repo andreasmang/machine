c2f303ec80bdbbb43361b832b0bd79340236dafa
Andreas Mang <andreas@math.uh.edu>
2023-03-09 13:45:26 -0600

    ++

--- istartup.m
+++ istartup.m
@@ -2,3 +2,12 @@ clear, close all;
 clc;
 
 addpath('core');
+
+
+
+%#######################################################
+% This code is part of the Matlab-based toolbox
+% MACHINE --- MAthematical and Computational metHods
+% for INverse problEms
+% For details see https://github.com/andreasmang/machine
+%#######################################################
